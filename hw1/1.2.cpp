a312c0a2666e2fff9a3baf224fa07fec466709ea
sdes9312 <sdes97106@gmail.com>
2025-07-23 00:55:44 +0800

    Update 1.2.cpp

--- hw1/1.2.cpp
+++ hw1/1.2.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int ackermann(int m, int n) {
+    stack<int> s;
+    s.push(m);
+
+    while (!s.empty()) {
+        m = s.top();
+        s.pop();
+
+        if (m == 0) {
+            n = n + 1;
+        } else if (n == 0) {
+            s.push(m - 1);
+            n = 1;
+        } else {
+            s.push(m - 1);
+            s.push(m);
+            n = n - 1;
+        }
+    }
+    return n;
+}
+
+int main() {
+    int m = 3, n = 4;
+    cout << "Ackermann(" << m << ", " << n << ") = " << ackermann(m, n) << endl;
+    return 0;
+}
 
