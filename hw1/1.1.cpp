f20226f8f4eb512c46f533d42c776d357050d6c3
sdes9312 <sdes97106@gmail.com>
2025-07-23 00:54:00 +0800

    Update 1.1.cpp

--- hw1/1.1.cpp
+++ hw1/1.1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int ackermann_recursive(int m, int n) {
+int ackermann(int m, int n) {
     if (m == 0) return n + 1;
-    else if (n == 0) return ackermann_recursive(m - 1, 1);
-    else return ackermann_recursive(m - 1, ackermann_recursive(m, n - 1));
+    else if (n == 0) return ackermann(m - 1, 1);
+    else return ackermann(m - 1, ackermann(m, n - 1));
 }
 
 int main() {
     int m = 2, n = 3;
-    cout << "Recursive Ackermann(" << m << ", " << n << ") = " << ackermann_recursive(m, n) << endl;
+    cout << "Ackermann(" << m << ", " << n << ") = " << ackermann(m, n) << endl;
     return 0;
 }
